fe85c8a150f4c67226c7855405e155baf90d5cf6
lecaoquochung <lecaoquochung@gmail.com>
2018-04-16 15:58:19 +0900

    add public file

---
+++ test/test_public1.py
@@ -0,0 +1,14 @@
+import glob, os, shlex, subprocess, sys
+
+app = os.environ['APP_COMMAND']
+
+def main(N):
+    return N * (4 ** (N-1))
+
+def check(prob):
+    assert main(int(prob)) == int(subprocess.check_output(shlex.split(app) + [prob]))
+
+def random_test():
+    '''Random test'''
+    for prob in xrange(1,31):
+        yield check, '{}'.format(prob)
