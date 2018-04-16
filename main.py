1d24f660b3f154d0bab9d8c8a106cfc956662ef8
lecaoquochung <lecaoquochung@gmail.com>
2018-04-16 15:40:05 +0900

    reset to codecheck_count_three

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import sys
-from operator import itemgetter
 
-def main(filename):
-    segments = []
-    with open(filename, 'r') as fp:
-        fp.readline()
-        segments = [[int(p) for p in s.strip().split(' ')] for s in fp.readlines()]
-    segments.sort(key=itemgetter(0))
-    ret = []
-    for seg in segments:
-        for r in ret:
-            if (seg[0] <= r[1]) and (seg[1] >= r[1]):
-                r[1] = seg[1]
-                break
-            elif (seg[0] <= r[0]) and (seg[1] >= r[0]):
-                r[0] = seg[0]
-                break
-        else:
-            ret.append(list(seg))
-    print max(map(lambda r: r[1] - r[0], ret))
+def main(N):
+    # BEGIN_CHALLENGE
+    print N * (4 ** (N-1))
+    # END_CHALLENGE
 
-main(sys.argv[1])
+main(int(sys.argv[1]))
--- test/test_private.py
+++ test/test_private.py
@@ -1,36 +1,14 @@
 import glob, os, shlex, subprocess, sys
-from operator import itemgetter
 
 app = os.environ['APP_COMMAND']
 
-def main(filename):
-    segments = []
-    with open(filename, 'r') as fp:
-        fp.readline()
-        segments = [[int(p) for p in s.strip().split(' ')] for s in fp.readlines()]
-    segments.sort(key=itemgetter(0))
-    ret = []
-    for seg in segments:
-        for r in ret:
-            if (seg[0] <= r[1]) and (seg[1] >= r[1]):
-                r[1] = seg[1]
-                break
-            elif (seg[0] <= r[0]) and (seg[1] >= r[0]):
-                r[0] = seg[0]
-                break
-        else:
-            ret.append(list(seg))
-    return max(map(lambda r: r[1] - r[0], ret))
+def main(N):
+    return N * (4 ** (N-1))
 
 def check(prob):
-    assert main(prob) == int(subprocess.check_output(shlex.split(app) + [prob]))
+    assert main(int(prob)) == int(subprocess.check_output(shlex.split(app) + [prob]))
 
 def random_test():
     '''Random test'''
-    for prob in glob.glob('./input/test_*.txt'):
-        yield check, prob
-
-def test_bridge():
-    '''Bridge test'''
-    prob = './input/bridge.txt'
-    assert main(prob) == int(subprocess.check_output(shlex.split(app) + [prob]))
+    for prob in xrange(1,31):
+        yield check, '{}'.format(prob)
